a55550c59154b60e12cca4c4a6744110bac462f5
Eugene S. Ostrovsky <eugene.ostrovsky@amalgamasimulation.ru>
2024-09-15 22:10:43 +0300

    Step 9: Queuing. ServiceWithResources

--- bundles/com.company.warehouse.simulation/src/com/company/warehouse/simulation/Dispatcher.java
+++ bundles/com.company.warehouse.simulation/src/com/company/warehouse/simulation/Dispatcher.java
@@ -1,63 +1,116 @@
 package com.company.warehouse.simulation;
 
 import com.amalgamasimulation.engine.Engine;
+import com.amalgamasimulation.service.ServiceWithResources;
+import com.amalgamasimulation.service.ServiceWithResources.ResourceSeizingRule;
 import com.company.warehouse.datamodel.Direction;
 import com.company.warehouse.simulation.equipment.Forklift;
 import com.company.warehouse.simulation.equipment.Truck;
 import com.company.warehouse.simulation.tasks.MovePalletTask;
 
+import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Objects;
+import java.util.Map;
 import java.util.Queue;
+import java.util.function.BiConsumer;
 
 public class Dispatcher {
 
     private final Engine engine;
     private final Model model;
+    private final Map<Direction, ServiceWithResources<Truck, Gate>> gates = new HashMap<>();
+
+    private final Map<Direction, Queue<Gate>> pendingGatesWithTrucks = Map.of(
+            Direction.IN, new LinkedList<>(),
+            Direction.OUT, new LinkedList<>()
+            );
+
     private final Queue<Forklift> availableForklifts = new LinkedList<>();
+    private final Queue<BiConsumer<Forklift, Runnable>> forkliftRequests = new LinkedList<>();
 
     public Dispatcher(Model model) {
         engine = model.engine();
         this.model = model;
-        availableForklifts.addAll(model.getForklifts());
+
+        model.getForklifts().forEach(f -> addAvailableForklift(f));
+
+        for (var d : Direction.values()) {
+            gates.put(d, new ServiceWithResources<>(engine, model.getGatesInDirection(d), ResourceSeizingRule.LEAST_USED));
+        }
+    }
+
+    private void addAvailableForklift(Forklift forklift) {
+        final var request = forkliftRequests.poll();
+
+        if (request != null) {
+            request.accept(forklift, () -> addAvailableForklift(forklift));
+        } else {
+            availableForklifts.add(forklift);
+        }
+    }
+
+    private void requestForklift(BiConsumer<Forklift, Runnable> request) {
+        final var forklift = availableForklifts.poll();
+
+        if (forklift != null) {
+            request.accept(forklift, () -> addAvailableForklift(forklift));
+        } else {
+            forkliftRequests.add(request);
+        }
     }
 
     public void truckArrived(Truck truck) {
-        final var gate = model.getGatesInDirection(truck.getDirection()).stream()
-                .filter(g -> g.getTruck().isEmpty())
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("No gate available to park " + truck));
-        handleTruckOnGate(truck, gate);
+        final var suitableGates = gates.get(truck.getDirection());
+        suitableGates.placeRequest(truck, (s, gate) ->
+            handleTruckOnGate(truck, gate, () ->
+                suitableGates.release(truck)
+            )
+        );
     }
 
-    private void handleTruckOnGate(Truck truck, Gate gate) {
+    private void handleTruckOnGate(Truck truck, Gate gate, Runnable onComplete) {
         final var direction = gate.getDirection();
         final var oppositeDirection = (direction == Direction.IN) ? Direction.OUT : Direction.IN;
 
         gate.parkTruck(truck);
 
-        model.getGatesInDirection(oppositeDirection).stream()
-                .filter(g -> g.getTruck().isPresent())
-                .findFirst()
-                .ifPresent(oppositeGate ->
-                    handleGatePair(gate, oppositeGate, Objects.requireNonNull(availableForklifts.poll(), "No available forklifts"))
-                );
+        final var oppositeGate = pendingGatesWithTrucks.get(oppositeDirection).poll();
+
+        if (oppositeGate == null) {
+            pendingGatesWithTrucks.get(direction).add(gate);
+            return;
+        }
+
+        handleGatePair(gate, oppositeGate, () -> {
+            gate.unparkTruck(truck);
+            onComplete.run();
+        });
     }
 
-    private void handleGatePair(Gate gate, Gate oppositeGate, Forklift forklift) {
+    private void handleGatePair(Gate gate, Gate oppositeGate, Runnable onComplete) {
         final var truck = gate.getTruck().get();
         final var oppositeTruck = oppositeGate.getTruck().get();
-        final boolean loading = gate.getDirection() == Direction.OUT;
+
+        requestForklift((forklift, forkliftReleaser) ->
+            handleGatePairWithForklift(truck, oppositeTruck, forklift, () -> {
+                forkliftReleaser.run();
+                oppositeGate.unparkTruck(oppositeTruck);
+                gates.get(oppositeTruck.getDirection()).release(oppositeTruck);
+                onComplete.run();
+            })
+        );
+    }
+
+    private void handleGatePairWithForklift(Truck truck, Truck oppositeTruck, Forklift forklift, Runnable onComplete) {
+        final boolean loading = truck.getDirection() == Direction.OUT;
 
         if (!truck.isAvailableFor(loading)) {
-            gate.unparkTruck(truck);
-            oppositeGate.unparkTruck(oppositeTruck);
-            availableForklifts.add(forklift);
+            onComplete.run();
             return;
         }
 
         newMovePalletTask(forklift, truck, oppositeTruck, loading).start(() ->
-            handleGatePair(gate, oppositeGate, forklift)
+            handleGatePairWithForklift(truck, oppositeTruck, forklift, onComplete)
         );
     }
 
